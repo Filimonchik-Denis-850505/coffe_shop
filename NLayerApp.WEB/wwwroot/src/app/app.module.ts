371eedf35195eab3911f83f851c81eef3cb41f92
Anton But-Gusaim(850505) <a.butgusaim1@mail.ru>
2020-10-16 23:55:46 +0300

    add routing on front

--- NLayerApp.WEB/wwwroot/src/app/app.module.ts
+++ NLayerApp.WEB/wwwroot/src/app/app.module.ts
@@ -1,21 +1,30 @@
 ﻿import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-import {AppComponent} from './app.component';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
+
+
+import {AppComponent} from './app.component';
 import {FooterComponent} from "./footer/footer.component";
 import {HeaderComponent} from "./header/header.component";
+
+import { AppRoutingModule } from './app-routing.module';
+import {HomeComponent} from "./home/home.component";
+
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        HttpClientModule
+        HttpClientModule,
+        AppRoutingModule,
+
     ],
     declarations: [
         AppComponent,
         HeaderComponent,
-        FooterComponent
+        FooterComponent,
+        HomeComponent
     ],
     bootstrap: [
         AppComponent
---
+++ NLayerApp.WEB/wwwroot/src/app/home/home-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+
+const routes: Routes = [
+    { path: 'catalog', loadChildren: () => import('./catalog/catalog.module').then(m => m.CatalogModule) },
+    { path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule) },
+    { path: '', redirectTo: 'main', pathMatch: 'full'}
+ ];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
