3cfff925f4264f1fb3329e06eaa4b693d5cc5cac
Anton But-Gusaim(850505) <a.butgusaim1@mail.ru>
2020-10-19 23:06:05 +0300

    change front

--- NLayerApp.WEB/wwwroot/src/app/app.module.ts
+++ NLayerApp.WEB/wwwroot/src/app/app.module.ts
@@ -8,7 +8,7 @@ import {AppComponent} from './app.component';
 import {FooterComponent} from "./footer/footer.component";
 import {HeaderComponent} from "./header/header.component";
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 import {HomeComponent} from "./home/home.component";
 
 
