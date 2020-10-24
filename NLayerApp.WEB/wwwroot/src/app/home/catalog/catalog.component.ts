eec43c8c01b643b2ebac7cd2b9b685485b33efa7
Anton But-Gusaim(850505) <a.butgusaim1@mail.ru>
2020-10-24 16:15:38 +0300

    add beta-catalog

--- NLayerApp.WEB/wwwroot/src/app/home/catalog/catalog.component.ts
+++ NLayerApp.WEB/wwwroot/src/app/home/catalog/catalog.component.ts
@@ -9,6 +9,9 @@ export class CatalogComponent implements OnInit {
   navBar = ['все', 'чай', 'кофе', 'травы'];
   selectedId: string = this.navBar[0];
 
+  tmpTitle = ['Name1', 'Name2','Name3','Name4','Name5', 'Name6','Name7','Name8'];
+  description: string = "dks hfkjsdh fjkhsdkj fhksdhfkj hsd kjfhk jsdhfk jhsd kjfhkj sdhf kjhsdk jfh";
+
   constructor() { }
 
   ngOnInit(): void {
