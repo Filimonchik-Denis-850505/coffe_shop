53d3e3cfae6c2c41336848995135cc3dc0d76e3b
Anton But-Gusaim(850505) <a.butgusaim1@mail.ru>
2020-10-24 16:15:38 +0300

    add beta-catalog

--- NLayerApp.WEB/wwwroot/src/app/home/catalog/catalog.component.ts
+++ NLayerApp.WEB/wwwroot/src/app/home/catalog/catalog.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./catalog.component.scss']
 })
 export class CatalogComponent implements OnInit {
+  navBar = ['все', 'чай', 'кофе', 'травы'];
+  selectedId: string = this.navBar[0];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSwitchNavItem(item: string){
+    this.selectedId = item;
+  }
 }
--- NLayerApp.WEB/wwwroot/src/app/home/catalog/catalog.module.ts
+++ NLayerApp.WEB/wwwroot/src/app/home/catalog/catalog.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CatalogRoutingModule } from './catalog-routing.module';
+import {CatalogComponent} from "./catalog.component";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CatalogComponent],
   imports: [
     CommonModule,
-    CatalogRoutingModule
+    CatalogRoutingModule,
   ]
 })
 export class CatalogModule { }
